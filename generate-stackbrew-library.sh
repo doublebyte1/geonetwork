53989b76930e92120f38110069bb9c8f284988e8
doublebyte1 <doublebyte@gmail.com>
2016-10-04 16:21:06 +0200

    - updated tag script to point to this repository

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -35,10 +35,10 @@ dirCommit() {
 }
 
 cat <<-EOH
-# this file is generated via https://github.com/GeoCat/geonetwork-official-images/blob/$(fileCommit "$self")/$self
+# this file is generated via https://github.com/doublebyte1/geonetwork/blob/non-official/$(fileCommit "$self")/$self
 
 Maintainers: Joana Simoes <joana.simoes@geocat.net> (@doublebyte1)
-GitRepo: https://github.com/geocat/geonetwork-official-images
+GitRepo: https://github.com/doublebyte1/geonetwork/non-official/
 EOH
 
 # prints "$2$1$3$1...$N"
